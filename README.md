78c177a856d6d70563a16c80b02cff74c1d00ea8
Parmjeet Kaur <work.technologies25@gmail.com>
2023-02-25 20:31:19 +1000

    done!

--- README.md
+++ README.md
@@ -316,7 +316,7 @@ parkWildlife();
 // src/ColoradoStateParks.js
 import React from "react";
 import howManyParks from "./parks/howManyParks";
-import MesaVerde from "./parks/MesaVerde";
+import MesaVerde from "./parks/MesaVerde"
 import * as RMFunctions from "./parks/RockyMountain";
 
 export default function ColoradoStateParks() {
--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,12 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {tree, wildlife} from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
+export default ColoradoStateParks;
